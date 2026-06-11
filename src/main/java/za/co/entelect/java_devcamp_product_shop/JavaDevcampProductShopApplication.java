6707f9d0fa50551dcb25060ccc8535352fbb62eb
vhugalaEntelect-123 <vhugala.kumbani@entelect.coza>
2026-06-11 10:02:05 +0200

    Initial commit

---
+++ src/main/java/za/co/entelect/java_devcamp_product_shop/JavaDevcampProductShopApplication.java
@@ -0,0 +1,13 @@
+package za.co.entelect.java_devcamp_product_shop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaDevcampProductShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaDevcampProductShopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/za/co/entelect/java_devcamp_product_shop/JavaDevcampProductShopApplicationTests.java
@@ -0,0 +1,13 @@
+package za.co.entelect.java_devcamp_product_shop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaDevcampProductShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
